9b3c11c60158dd0edde21fd39ade1ec0cb66d5c9
takamasakimura <ryugakimura@icloud.com>
2025-07-25 13:42:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ aa_blocks = [html.unescape(dd.get_text("\n")) for dd in dd_blocks]
 full_text = "\n\n".join(aa_blocks)
 
 # ▼ .woff2フォント読み込み＆Base64エンコード
-font_path = os.path.join("static", "MS-PGothic.woff2")
+font_path = os.path.join("static", "MS-UIGothic.woff2")
 with open(font_path, "rb") as font_file:
     woff2_base64 = base64.b64encode(font_file.read()).decode("utf-8")
 
